234468eef06e2ae8b01140706ee124c8aec9d4ff
deepakChauhanCloud <deepak.chauhan@cloudstok.com>
2023-10-05 19:40:29 +0530

    login frontend

--- client/nwdaf/src/App.js
+++ client/nwdaf/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import Axios from 'axios';
+import Form from './components/form/Form';
 
-function App() {
+const App = () => {
+//   const [data, setData] = useState("")
+//   const getData= async() => {
+//     const res = await Axios.get("http://localhost:5000/getData");
+//     setData(res.data)
+//   }
+//  useEffect(()=>{
+//   getData()
+//  },[]);
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Form/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
